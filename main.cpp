81fc6420bcf9000b821ef20a6dd17abcd6472c85
Aashish Ghimire <aashishghimire9999@gmail.com>
2019-01-02 15:35:38 +0545

    First commit

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+int main()
+{std::cout<<"Hello Worldhbh"<<std::endl;
+    /* code */
+    return 0;
+}
---
+++ main2.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello World2122"<<endl;
+    /* code */
+    return 0;}
